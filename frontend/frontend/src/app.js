1bb3430084d382334673e53393fa53f60cdd3c73
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2025-05-24 17:08:56 +0200

    File Structure

---
+++ frontend/frontend/src/app.js
@@ -0,0 +1,32 @@
+//This file is where you will setup your Routing.
+
+//lines 4-12 is just a placeholder for now.
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+/*import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+class app extends React.Component 
+{
+  render() 
+  {
+    return (
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default app;*/
