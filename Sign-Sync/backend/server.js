07640723586240e3d1e0e778e571ded13bd330a5
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2025-05-26 22:30:19 +0200

    Register API
    I have tested it in Postman and it works 100%.

--- Sign-Sync/backend/server.js
+++ Sign-Sync/backend/server.js
@@ -1,27 +1,30 @@
 import express from 'express';
 import path from 'path';
 import { MongoClient } from 'mongodb';
+import apiRoutes from './userApi';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// MongoDB connection string
+//MongoDB connection string
 const url = 'mongodb+srv://Jamean:g19Apo11oProjects@sign-sync.wxahktp.mongodb.net/'; //this is unsafe, but for development purposes only
-const dbName = 'Sign-Sync';
+const dbName = 'SIgn-Sync';
 
-// Middleware
+//Middleware
 app.use(express.json());
 
-// Serve frontend files
+//Serve frontend files
 const publicPath = path.resolve('frontend', 'public');
 app.use(express.static(publicPath));
 
-// Fallback for React Router
+app.use('/userApi', apiRoutes);
+
+//Fallback for React Router
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
-// MongoDB connection and server start
+//MongoDB connection and server start
 async function main() 
 {
   const client = new MongoClient(url);
@@ -35,6 +38,12 @@ async function main()
     const db = client.db(dbName);
 
     //console.log('Available collections:', await db.listCollections().toArray()); //debugging
+
+    const userCollection = db.collection('Users');
+    
+    //console.log('User collection:', await userCollection.find({}).toArray()); //debugging
+    
+    app.locals.userCollection = userCollection;
 
     app.listen(port, () => {
       console.log(`Server running at http://localhost:${port}`);
---
+++ Sign-Sync/backend/userApi.js
@@ -0,0 +1,62 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => 
+{
+    const { username, email, password } = req.body;
+
+    try
+    {
+        const existingUsername = await req.app.locals.userCollection.findOne({ username })
+        const existingEmail = await req.app.locals.userCollection.findOne({ email })
+
+        if(existingUsername) 
+        {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+
+        if(existingEmail)
+        {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+
+        const latestUser = await req.app.locals.userCollection
+                            .find({})
+                            .sort({userID: -1})
+                            .limit(1)
+                            .toArray();
+        
+        let newUserID;
+
+        if(latestUser.length>0)
+        {
+            newUserID = latestUser[0].userID+1;
+        }
+        else
+        {
+            newUserID = 1;
+        }
+
+        const newUser = {
+            userID: newUserID,
+            username,
+            email,
+            password
+        };
+
+        await req.app.locals.userCollection.insertOne(newUser);
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'signup successful',
+        });
+    } 
+    catch(error) 
+    {
+        res.status(500).json({ message: 'Error signing up user', error: error.message });
+    }
+    
+}); 
+
+export default router;
