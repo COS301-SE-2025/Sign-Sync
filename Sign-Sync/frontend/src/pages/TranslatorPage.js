10f894e9ac9212dfc37ddf2398ab75a578060f41
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-05-27 16:30:08 +0200

    Text to sign working
    The page for text to sign works perfectly, styling needed

--- Sign-Sync/frontend/src/pages/TranslatorPage.js
+++ Sign-Sync/frontend/src/pages/TranslatorPage.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Camera from "../components/Camera";
 import SideNavbar from "../components/sideNavbar";
+import textToSign from "../components/textToSign";
+import TextToSign from "../components/textToSign";
 
 class TranslatorPage extends React.Component
 {
     render()
     {
         return (
-            <Camera />
+            <div className={"flex flex-row"}>
+                <SideNavbar/>
+                <TextToSign/>
+            </div>
         )
     }
 }
