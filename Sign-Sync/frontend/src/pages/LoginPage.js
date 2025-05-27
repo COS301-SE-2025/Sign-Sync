23c576fef4aeadf101b4a783537b951a8d54478c
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2025-05-27 20:28:35 +0200

    relative height Login CSS
    Gonna commit so that I can test this on my laptop.

--- Sign-Sync/frontend/src/pages/LoginPage.js
+++ Sign-Sync/frontend/src/pages/LoginPage.js
@@ -62,11 +62,12 @@ class LoginPage extends React.Component
 
         return (
             <div className="flex relative justify-center items-center w-screen h-screen bg-blue-900">
-                <div className="absolute bg-sky-950 h-[906px] rounded-[55px] w-[1237px] z-[1] max-md:h-4/5 max-md:w-[90%] max-sm:h-3/4 max-sm:w-[95%]" />
+                {/* <div className="absolute bg-sky-950 h-[906px] rounded-[55px] w-[1237px] z-[1] max-md:h-4/5 max-md:w-[90%] max-sm:h-3/4 max-sm:w-[95%]" /> */}
+                <div className="absolute inset-0 m-auto bg-sky-950 w-[90%] max-w-[1237px] h-full rounded-[55px] max-md:rounded-[40px] max-sm:rounded-[30px]" />
 
                 <form
                     onSubmit={this.handleSubmit}
-                    className="box-border flex relative flex-col gap-12 items-start p-12 bg-white rounded-xl border border-amber-50 border-solid h-[796px] min-w-80 w-[773px] z-[2] max-md:gap-8 max-md:p-8 max-md:h-auto max-md:max-w-[600px] max-md:w-[90%] max-sm:gap-6 max-sm:p-6 max-sm:max-w-[400px] max-sm:w-[95%]"
+                    className="box-border flex relative flex-col gap-12 items-start p-12 bg-white rounded-xl border border-amber-50 border-solid h-fit max-h-screen overflow-auto min-w-80 w-[773px] z-[2] max-md:gap-8 max-md:p-8 max-md:h-auto max-md:max-w-[600px] max-md:w-[90%] max-sm:gap-6 max-sm:p-6 max-sm:max-w-[400px] max-sm:w-[95%]"
                 >
                     <div className="flex flex-col gap-4 items-start w-full">
                         <label className="self-stretch text-3xl font-bold leading-10 text-stone-900 max-md:text-3xl max-md:leading-9 max-sm:text-2xl max-sm:leading-8">
