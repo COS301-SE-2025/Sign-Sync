062ecb1a80d735e287ff394c8054ed9eee6e2bba
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-05-27 21:06:38 +0200

    Styled the camera mode

--- Sign-Sync/frontend/src/pages/TranslatorPage.js
+++ Sign-Sync/frontend/src/pages/TranslatorPage.js
@@ -1,22 +1,42 @@
 import React from "react";
 import Camera from "../components/Camera";
 import SideNavbar from "../components/sideNavbar";
-import SpeechToTextBox from "../components/SpeechToTextBox";
+import TextToSign from "../components/textToSign";
+import SpeechToTextBox from "../components/speechToTextBox";
 
 class TranslatorPage extends React.Component
 {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: "SignText"
+        }
+    }
+
+    changeType = () => {
+        {this.state.type === "SignText" ? this.setState({type: "TextSign"}) : this.setState({type: "SignText"})}
+    }
+
     render()
     {
+        let translatorMode;
+        let translatorType = this.state.type;
+        if(translatorType === "SignText"){
+            translatorMode = <div>
+                <Camera/>
+            </div>
+        }else{
+            translatorMode = <div>
+                <SpeechToTextBox/>
+                <TextToSign/>
+            </div>
+        }
         return (
-            // <Camera />
-            <div className="flex flex-row h-screen">
-                <SideNavbar />
-
-                <div className="flex-1 p-4 overflow-auto">
-                    <Camera />
-
-                    {/* textbook with mic button */}
-                    <SpeechToTextBox />
+            <div className={"flex h-screen"}>
+                <SideNavbar/>
+                <div className={"flex-1 flex items-center justify-center bg-gray-100"}>
+                    {translatorMode}
+                    <button onClick={this.changeType}>Switch mode</button>
                 </div>
             </div>
         )
