7f62d086084fb3242058cd248df06dbb8712cb3e
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-05-27 21:49:14 +0200

    Translator type swapping
    The user can click the buttons at bottom of the screen to swap between the translator types. CSS changes dynamically with it

--- Sign-Sync/frontend/src/pages/TranslatorPage.js
+++ Sign-Sync/frontend/src/pages/TranslatorPage.js
@@ -3,6 +3,7 @@ import Camera from "../components/Camera";
 import SideNavbar from "../components/sideNavbar";
 import TextToSign from "../components/textToSign";
 import SpeechToTextBox from "../components/speechToTextBox";
+import Swap from "../assets/Swap-icon.png"
 
 class TranslatorPage extends React.Component
 {
@@ -34,9 +35,19 @@ class TranslatorPage extends React.Component
         return (
             <div className={"flex h-screen"}>
                 <SideNavbar/>
-                <div className={"flex-1 flex items-center justify-center bg-gray-100"}>
+                <div className={"flex-1 flex flex-col items-center justify-center bg-gray-100"}>
                     {translatorMode}
-                    <button onClick={this.changeType}>Switch mode</button>
+                    <div className={"flex mt-2"}>
+                        <button onClick={this.changeType} className={`p-2 font-bold w-20 ${translatorType === "SignText" ? "bg-[#801E20] text-[#FFFFFD]": "bg-[#FFFFFD] text-[#801E20]"}  border-gray-600 rounded`}>
+                            Sign
+                        </button>
+                        <button className="px-4" onClick={this.changeType}>
+                            <img src={Swap} alt={"Swap"} className="w-8 h-8"/>
+                        </button>
+                        <button onClick={this.changeType} className={`p-2 font-bold w-20 ${translatorType === "SignText" ? "bg-[#FFFFFD] text-[#801E20]" : "bg-[#801E20] text-[#FFFFFD]"} border-gray-600 rounded`}>
+                            Speech
+                        </button>
+                    </div>
                 </div>
             </div>
         )
