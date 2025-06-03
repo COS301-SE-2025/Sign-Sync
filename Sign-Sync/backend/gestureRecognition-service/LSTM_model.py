12a8a601a146cf2133f81f1045486dbd53ea37fa
WJvdwalt <104898337+WJvdwalt@users.noreply.github.com>
2025-06-03 04:48:39 +0200

    Regularization in LSTM model decreased and script for prediction started
    This regularization of 0.0005 from 0.001 lead to an accuracy of 84% over 100 epochs, but validation accuracy < 10%. Need to add dropout and review LSTM architecture

--- Sign-Sync/backend/gestureRecognition-service/LSTM_model.py
+++ Sign-Sync/backend/gestureRecognition-service/LSTM_model.py
@@ -5,10 +5,10 @@ from tensorflow.keras.regularizers import l2
 def build_model(input_shape, num_classes):
     model = Sequential([
         Masking(mask_value=0.0, input_shape=input_shape),
-        LSTM(128, return_sequences=True, kernel_regularizer=l2(0.001)),
-        LSTM(64, kernel_regularizer=l2(0.001)),
-        Dense(64, activation='relu', kernel_regularizer=l2(0.001)),
-        Dense(num_classes, activation='softmax', kernel_regularizer=l2(0.001))
+        LSTM(128, return_sequences=True, kernel_regularizer=l2(0.00005)),
+        LSTM(64, kernel_regularizer=l2(0.00005)),
+        Dense(64, activation='relu', kernel_regularizer=l2(0.00005)),
+        Dense(num_classes, activation='softmax', kernel_regularizer=l2(0.00005))
     ])
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
