baf57806089df11cee50d43f456aec39add4607f
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2025-06-07 22:58:34 +0200

    Made the Landing Page the root page

--- Sign-Sync/frontend/src/app.js
+++ Sign-Sync/frontend/src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import Translator from "./pages/TranslatorPage";
@@ -13,7 +14,7 @@ class app extends React.Component
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Translator />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/translator" element={<Translator />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegistrationPage />} />
