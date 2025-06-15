75dda6b47f77604cc6a3216bdab0fcf7f800154b
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-06-15 14:56:30 +0200

    Imported the avatar file
    Blender model is visible in the viewport. Has an artifact that has to be fixed

--- Sign-Sync/frontend/src/components/AvatarViewport.js
+++ Sign-Sync/frontend/src/components/AvatarViewport.js
@@ -1,18 +1,19 @@
 import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, useAnimations, OrbitControls } from '@react-three/drei';
 import { MeshStandardMaterial, Color } from 'three';
+import TranslatorAvatar from '../assets/3DModels/Avatar.glb';
+
 function Avatar() {
     const avatarReference = useRef();
-    return (
-        <mesh ref={avatarReference} position={[0,0,0]}>
-        </mesh>
-    );
+    const {scene} = useGLTF(TranslatorAvatar);
+    return <primitive ref={avatarReference} object={scene} position={[0,-2,3]} />;
 }
 
 export default function AvatarViewport() {
     return (
         <Canvas style={{ height: '50vh', background: '#222' }}>
-            <ambientLight intensity={0.5} />
+            <ambientLight intensity={0.75} />
             <pointLight position={[5,5,5]} />
             <Avatar />
         </Canvas>
