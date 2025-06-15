e9deb3b6ad58eb80718c9cbfe4d81dc2ec0cdcf0
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-06-15 21:24:33 +0200

    Avatar now plays an action when the user inputs certain strings
    The input is split into words and if the input is correct, then the corresponding animation is played

--- Sign-Sync/frontend/src/components/AvatarViewport.js
+++ Sign-Sync/frontend/src/components/AvatarViewport.js
@@ -1,29 +1,49 @@
 import React, {useEffect, useRef} from 'react';
 import { Canvas } from '@react-three/fiber';
-import { useGLTF, useAnimations, OrbitControls } from '@react-three/drei';
-import {MeshStandardMaterial, Color, DirectionalLight, AmbientLight} from 'three';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import {DirectionalLight, AmbientLight} from 'three';
 import TranslatorAvatar from '../assets/3DModels/Avatar.glb';
 
-function Avatar({input}) {
-    console.log({input});
+function Avatar({signs}) {
     const avatarReference = useRef();
     const {scene, animations} = useGLTF(TranslatorAvatar);
     const {actions} = useAnimations(animations,avatarReference);
+
     useEffect(() => {
         const sun = new DirectionalLight('rgb(255,255,255)',1);
         sun.position.set(5,10,7.5);
         scene.add(sun);
         scene.add(new AmbientLight(0xffffff,0.75));
-        actions['NodsHead']?.play();
-    }, [actions]);
+    }, [scene]);
+
+    useEffect(() => {
+        if(signs.length > 0){
+            for(let i = 0; i < signs.length; i++){
+                actions[signs[i]]?.play();
+            }
+        }
+    }, [actions,signs]);
 
     return <primitive ref={avatarReference} object={scene} position={[0,-2,3]} />;
 }
 
 export default function AvatarViewport({input}) {
+    const words = input.split(' ');
+    const signs = [];
+    //Fast API connection will happen here but this will be implemented later
+    for (let i = 0; i < words.length; i++) {
+        if(words[i] === "nod"){
+            signs.push("NodsHead");
+
+        }
+        if(words[i] === "shake"){
+            signs.push("ShakesHead");
+        }
+    }
+
     return (
         <Canvas style={{ height: '50vh', background: '#222' }}>
-            <Avatar input={input}/>
+            <Avatar signs={signs}/>
         </Canvas>
     );
 }
