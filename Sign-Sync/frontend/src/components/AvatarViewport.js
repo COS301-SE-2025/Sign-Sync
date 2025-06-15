2f23e1962f26ed4167040f8cf1f16bb107f6d5ce
Itsyaboijeff01 <u23545977@tuks.co.za>
2025-06-15 16:43:11 +0200

    Changed THREE JS environment to industry standard
    Instead of setting up the light sources in the html canvas function, it is done in the three js scene, allowing for better control.

--- Sign-Sync/frontend/src/components/AvatarViewport.js
+++ Sign-Sync/frontend/src/components/AvatarViewport.js
@@ -1,21 +1,22 @@
 import React, { useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useGLTF, useAnimations, OrbitControls } from '@react-three/drei';
-import {MeshStandardMaterial, Color, DirectionalLight} from 'three';
+import {MeshStandardMaterial, Color, DirectionalLight, AmbientLight} from 'three';
 import TranslatorAvatar from '../assets/3DModels/Avatar.glb';
 
 function Avatar() {
     const avatarReference = useRef();
     const {scene} = useGLTF(TranslatorAvatar);
-    scene.add(new DirectionalLight())
+    const sun = new DirectionalLight('rgb(255,255,255)',1);
+    sun.position.set(5,10,7.5);
+    scene.add(sun);
+    scene.add(new AmbientLight(0xffffff,0.75));
     return <primitive ref={avatarReference} object={scene} position={[0,-2,3]} />;
 }
 
 export default function AvatarViewport() {
     return (
         <Canvas style={{ height: '50vh', background: '#222' }}>
-            <ambientLight intensity={0.75} />
-            <directionalLight position={[5,10,7.5]} />
             <Avatar />
         </Canvas>
     );
