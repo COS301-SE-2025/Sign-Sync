ef2d6110dbd7bb3d4965b5fa1b19051b014e3edb
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2025-06-20 18:39:50 +0200

    Get preferences endpoint
    This works, I tested it with postman.

--- Sign-Sync/backend/auth-service/userApi.js
+++ Sign-Sync/backend/auth-service/userApi.js
@@ -83,5 +83,30 @@ router.post('/login', async (req, res) =>
     }
 });
 
+router.get('/preferences/:userID', async (req, res) => 
+{
+    const { userID } = req.params;
+
+    try 
+    {
+        const user = await req.app.locals.userCollection.findOne({ userID: parseInt(userID) });
+
+        if(!user) 
+        {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        //console.log("Fetched user for preferences:", user);
+
+        res.status(200).json({
+            status: 'success',
+            preferences: user.preferences || {},
+        });
+    } 
+    catch(error) 
+    {
+        res.status(500).json({ message: 'Error fetching preferences', error: error.message });
+    }
+});
 
 export default router;
