311ed685f29422e9cec9362ea0495cde00893946
StefanDramaLlama <138465598+StefanDramaLlama@users.noreply.github.com>
2025-06-23 13:07:54 +0200

    Fixed routing AccountSettings

--- Sign-Sync/frontend/src/pages/articles/AccountSettings.js
+++ Sign-Sync/frontend/src/pages/articles/AccountSettings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SideNavbar from "../../components/sideNavbar";
 import { FiUser, FiEye, FiMoon, FiAward, FiGlobe } from 'react-icons/fi';
 
 const AccountSettings = () => {
