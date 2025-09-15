a5391c9c1e85d0937ba6cbba1be485178baa34df
WJvdwalt <104898337+WJvdwalt@users.noreply.github.com>
2025-09-15 11:51:04 +0200

    Implemented rate limiting for http requests to the API gateway
    Rate limiting is implemented to allow for a maximum of 25 requests per minute  per IP address

--- Sign-Sync/backend/wordPrediction-service/API.py
+++ Sign-Sync/backend/wordPrediction-service/API.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-    ],  # add your dev frontend origins
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
