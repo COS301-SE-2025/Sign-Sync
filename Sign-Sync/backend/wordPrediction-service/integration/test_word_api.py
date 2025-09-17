368db35259f461ae292d91a37496d6d9f6793213
WJvdwalt <104898337+WJvdwalt@users.noreply.github.com>
2025-09-17 12:44:26 +0200

    Finished integration testing for the wordPrediciton service, specifically for the /translate endpoint

--- Sign-Sync/backend/wordPrediction-service/integration/test_word_api.py
+++ Sign-Sync/backend/wordPrediction-service/integration/test_word_api.py
@@ -1,6 +1,9 @@
 import pytest
 import requests
 
+# Just note that this testing must be run with "python -m pytest -v -s"
+# This is opposed to the unit testing which can just be run like normla python files
+
 ENDPOINT = "http://localhost:8006"
 
 def test_predict():
@@ -12,4 +15,22 @@ def test_predict():
     response = requests.post(ENDPOINT + "/predict", json=payload)
     assert response.status_code == 422
 
+def test_translate():
+    payload = {"text": ""}
+    response = requests.post(ENDPOINT + "/translate", json=payload)
+    data = response.json()
+    assert data["translation"] == ""
+    assert response.status_code == 200
+
+    payload = {"text": "       "}
+    response = requests.post(ENDPOINT + "/translate", json=payload)
+    data = response.json()
+    assert data["translation"] == ""
+    assert response.status_code == 200
+
+    payload = {"text": "STORE I GO"}
+    response = requests.post(ENDPOINT + "/translate", json=payload)
+    data = response.json()
+    assert data["translation"] == "I go to the store."
+    assert response.status_code == 200
 
