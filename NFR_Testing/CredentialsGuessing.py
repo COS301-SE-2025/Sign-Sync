9dc1e895bf8107202bcc93780e73cc511f0f7ee3
WJvdwalt <104898337+WJvdwalt@users.noreply.github.com>
2025-09-26 15:54:24 +0200

    Testing for login rate limiting finished
    Included testing for retry

--- NFR_Testing/CredentialsGuessing.py
+++ NFR_Testing/CredentialsGuessing.py
@@ -1,9 +1,10 @@
 import requests
 import pytest
+import time
 
 ENDPOINT = "http://localhost:3000/userApi/login"
-
-
+executed_tests = []
+   
 def test_login_rate_limit():
     payload = {"email": "a@gmail.com", "password": "wrongpassword"}
 
@@ -12,4 +13,8 @@ def test_login_rate_limit():
 
     response = requests.post(ENDPOINT, json=payload)
     assert response.status_code == 429
+
+    time.sleep(61)
+    response = requests.post(ENDPOINT, json=payload)
+    assert response.status_code == 400
 
